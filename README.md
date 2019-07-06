1dd29076e6d78400d5a343b573d78f1abd85c407
ishizuka yohsuke <ishizuka@nemui.jp>
2019-07-07 00:43:15 +0900

    ちゃんと動くように

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # slackevent
 slack event を見る人
+
+## 動くまで
+
+https://api.slack.com/apps から `Create New App` する
+`Basin Information` から `Verification Token` をメモっておく
+`Incoming Webhooks` を On にして `Add New Webhook to Workspace` から追加して、webhook url をメモっておく
+
+`.chalice/config-dist.json` を参考に `config.json` を編集する
+メモしておいた verification_code と webhook url を入れる
+
+デプロイする
+```
+$ chalice deploy
+```
+URLが表示されるので、これをメモしておく
+
+`Event Subscriptions` を On にして `Add Workspace Event` から `emoji_changed` を追加する
+
+`Request URL` にデプロイしたURLを入れる
+
+
--- app.py
+++ app.py
@@ -1,29 +1,69 @@
 from chalice import Chalice
 
+import os
+import json
+import urllib.request
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 app = Chalice(app_name='slackevent')
 
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return {'hello': 'world'}
 
+@app.route('/', methods=['POST'])
+def hoge():
+    data = app.current_request.json_body
+    if (data['type'] == 'url_verification'):
+        return url_verification(data)
+    if (data['type'] == 'event_callback'):
+        if (data['event']['type'] == 'emoji_changed'):
+            return emoji_changed(data['event'])
+    log.warn("unsupported data %s" % data)
+    return {
+        'unsupported': data
+    }
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+def url_verification(data):
+    if (data['token'] == os.environ["VERIFICATION_TOKEN"]):
+        return {
+            'challenge': data["challenge"]
+        }
+
+    return {}
+
+def emoji_changed(event):
+    if event['subtype'] == 'add':
+        """
+        data sample (https://api.slack.com/events/emoji_changed)
+        {
+            "type": "emoji_changed",
+            "subtype": "add",
+            "name": "picard_facepalm",
+            "value": "https://my.slack.com/emoji/picard_facepalm/db8e287430eaa459.gif",
+            "event_ts" : "1361482916.000004"
+        }
+        """
+        post = {
+            'attachments': [
+                {
+                    'title': event['name'],
+                    'image_url': event['value']
+                }
+            ]
+        }
+        url = os.environ["SLACK_WEBHOOK_EMOJI_CHANGED_URL"]
+        post_slack(url, post)
+    return {}
+
+def post_slack(url, data):
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    req = urllib.request.Request(url, json.dumps(data).encode(), headers)
+    with urllib.request.urlopen(req) as res:
+        body = res.read()
+    return
